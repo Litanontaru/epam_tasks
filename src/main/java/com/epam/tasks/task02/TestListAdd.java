cb765a841e1a6a7c3892bdbe6bb9333643ce2ea8
Komarov Vasiliy <komarov.vasiliy884@gmail.com>
2017-10-08 20:09:20 +0400

    The class 'TestListAdd' was modified. The list filling and test for adding elements were fixed.

--- src/main/java/com/epam/tasks/task02/TestListAdd.java
+++ src/main/java/com/epam/tasks/task02/TestListAdd.java
@@ -1,6 +1,6 @@
 package com.epam.tasks.task02;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -9,18 +9,32 @@ import java.util.function.Predicate;
  */
 public class TestListAdd {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(8);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        List<Integer> list = Arrays.asList(1, 2, 8, 3, 4, 5);
 
         Predicate<Integer> predicate = (i) -> i % 2 == 0;
 
         List<Integer> list2 = new PredicateList<>(list, predicate);
-        list2.add(5); // successful
-        list2.add(8); // exception
+
+        printElements(list2);
+        System.out.println();
+
+        checkAdd(list2.add(5));
+        checkAdd(list2.add(8));
+
+        System.out.println();
+        printElements(list2);
+    }
+
+    private static void printElements(List list){
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println("Size: " + list.size());
+    }
+
+    private static void checkAdd(boolean resultAdd){
+        if (resultAdd) System.out.println("Element has been added");
+        else System.out.println("Element hasn't been added");
     }
 }
