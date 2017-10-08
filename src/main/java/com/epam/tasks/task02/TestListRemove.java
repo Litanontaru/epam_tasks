d3b2ed7669acf146ac86cb31002973295797ab06
Komarov Vasiliy <komarov.vasiliy884@gmail.com>
2017-10-08 20:23:58 +0400

    The class 'TestListRemove' was modified. The list filling and test for removing elements were fixed.

--- src/main/java/com/epam/tasks/task02/TestListRemove.java
+++ src/main/java/com/epam/tasks/task02/TestListRemove.java
@@ -1,6 +1,6 @@
 package com.epam.tasks.task02;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -10,18 +10,36 @@ import java.util.function.Predicate;
 public class TestListRemove {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(8);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        List<Integer> list = Arrays.asList(1, 2, 8, 3, 4, 5);
 
         Predicate<Integer> predicate = (i) -> i % 2 == 0;
 
         List<Integer> list2 = new PredicateList<>(list, predicate);
-        list2.remove(0); // successful
-        list2.remove(1); // exception
+        printElements(list2);
+        //1, 2, 8, 3, 4, 5
+
+        list2.remove(0); // value: '1'. has been removed
+        list2.remove(1); // value: '2'. hasn't been removed
+
+        System.out.println();
+        printElements(list2);
+        //2, 8, 3, 4, 5
+
+        list2.remove(new Object()); // some object. hasn't been removed
+        list2.remove(list2.get(3)); // value: '4'. hasn't been removed
+        list2.remove(list2.get(4)); // value: '5'. has been removed
+
+        System.out.println();
+        printElements(list2);
+        //2, 8, 3, 4
     }
+
+    private static void printElements(List list){
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println("Size: " + list.size());
+    }
+
 }
